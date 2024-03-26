fc4d5718e5ecb2ade4f41b725a62954af4e4716a
Kirill <pro100normo@mail.ru>
2024-03-26 13:38:06 +0700

    Car engine torque curve

--- Assets/Scripts/Car.cs
+++ Assets/Scripts/Car.cs
@@ -3,25 +3,37 @@ using UnityEngine;
 [RequireComponent(typeof(CarChassis))]
 public class Car : MonoBehaviour
 {
-    [SerializeField] private float _maxMotorTorque;
+    // [SerializeField] private float _maxMotorTorque;
     [SerializeField] private float _maxSteerAngle;
     [SerializeField] private float _maxBrakeTorque;
 
+    [SerializeField] private AnimationCurve _engineTorqueCurve;
+    [SerializeField] private float _maxSpeed;
+
     private CarChassis _chassis;
     
-    //DEBUG ONLY
+    [Header("Debug Only")]
+    public float linearVelocity;
     public float throttleControl;
     public float steerControl;
     public float brakeControl;
 
-    private void Awake()
+    private void Start()
     {
         _chassis = GetComponent<CarChassis>();
     }
 
-    private void FixedUpdate()
+    private void Update()
     {
-        _chassis.motorTorque = throttleControl * _maxMotorTorque;
+        linearVelocity = _chassis.LinearVelocity;
+        float engineTorque = _engineTorqueCurve.Evaluate(_chassis.LinearVelocity);
+
+        if (_chassis.LinearVelocity >= _maxSpeed)
+        {
+            engineTorque = 0;
+        }
+
+        _chassis.motorTorque = throttleControl * engineTorque;
         _chassis.steerAngle = steerControl * _maxSteerAngle;
         _chassis.brakeTorque = brakeControl * _maxBrakeTorque;
     }
