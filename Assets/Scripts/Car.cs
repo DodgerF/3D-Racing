b34edee36c63094c312bed8113f10aeaae208d89
Kirill <pro100normo@mail.ru>
2024-03-29 17:11:48 +0700

    Gear

--- Assets/Scripts/Car.cs
+++ Assets/Scripts/Car.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 [RequireComponent(typeof(CarChassis))]
@@ -6,8 +7,20 @@ public class Car : MonoBehaviour
     [SerializeField] private float _maxSteerAngle;
     [SerializeField] private float _maxBrakeTorque;
 
+
+    [Header("Engine")]
     [SerializeField] private AnimationCurve _engineTorqueCurve;
-    [SerializeField] private float _maxMotorTorque;
+    [SerializeField] private float _engineMaxTorque;
+    [SerializeField] private float _engineMinRpm;
+    [SerializeField] private float _engineMaxRpm;
+    [SerializeField] private float _upShiftEngineRpm;
+    [SerializeField] private float _downShigftEngineRpm;
+
+    [Header("Gearbox")]
+    [SerializeField] private float[] _gears;
+    [SerializeField] private float _finalDriveRatio;
+
+
     [SerializeField] private float _maxSpeed;
 
     public float LinearVelocity => _chassis.LinearVelocity;
@@ -21,6 +34,12 @@ public class Car : MonoBehaviour
     public float throttleControl;
     public float steerControl;
     public float brakeControl;
+    public float engineTorque;
+    [SerializeField] private float _engineRpm;
+    [SerializeField] private float _selectedGear;
+    [SerializeField] private float _rearGear;
+    [SerializeField] private int _selectedGearIndex;
+
 
     private void Start()
     {
@@ -30,9 +49,11 @@ public class Car : MonoBehaviour
     private void Update()
     {
         linearVelocity = LinearVelocity;
-        float engineTorque = _engineTorqueCurve.Evaluate(LinearVelocity / _maxSpeed) * _maxMotorTorque;
+        
+        UpdateEngineTorque();
+        AutoGearShift();
 
-        if (_chassis.LinearVelocity >= _maxSpeed)
+        if (LinearVelocity >= _maxSpeed)
         {
             engineTorque = 0;
         }
@@ -40,5 +61,57 @@ public class Car : MonoBehaviour
         _chassis.motorTorque = throttleControl * engineTorque;
         _chassis.steerAngle = steerControl * _maxSteerAngle;
         _chassis.brakeTorque = brakeControl * _maxBrakeTorque;
+    }
+
+    private void UpdateEngineTorque()
+    {
+        _engineRpm = _engineMinRpm + Mathf.Abs(_chassis.GetAvarageRpm() * _selectedGear * _finalDriveRatio);
+        _engineRpm = Mathf.Clamp(_engineRpm, _engineMinRpm, _engineMaxRpm);
+
+        engineTorque = _engineTorqueCurve.Evaluate(_engineRpm / _engineMaxRpm) * _engineMaxTorque * _finalDriveRatio 
+                                                                                * Math.Sign(_selectedGear) * _gears[0];
+    }
+
+
+#region GearBox
+    private void AutoGearShift()
+    {
+        if(_selectedGear < 0) return;
+
+        if (_engineRpm >= _upShiftEngineRpm)
+        {
+            UpGear();
+        }
+        if (_engineRpm < _downShigftEngineRpm)
+        {
+            DownGear();
+        }
+    } 
+    public void UpGear()
+    {
+        ShiftGear(_selectedGearIndex + 1);
+    }
+    public void DownGear()
+    {
+        ShiftGear(_selectedGearIndex - 1);
+    }
+    public void ShiftToReverseGear()
+    {
+        _selectedGear = _rearGear;
+    }
+    public void ShiftToFirstGear()
+    {
+        ShiftGear(0);
+    }
+    public void ShiftToNetral()
+    {
+        _selectedGear = 0;
+    }
+#endregion
+    private void ShiftGear(int gearIndex)
+    {
+        gearIndex = Mathf.Clamp(gearIndex, 0, _gears.Length - 1);
+        _selectedGear = _gears[gearIndex];
+        _selectedGearIndex = gearIndex;
     }
 }
--- Assets/Scripts/CarInputControl.cs
+++ Assets/Scripts/CarInputControl.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 
 public class CarInputControl : MonoBehaviour
 {
-    [SerializeField] private Car car;
+    [SerializeField] private Car _car;
     [SerializeField] private AnimationCurve _brakeCurve;
     [SerializeField] private AnimationCurve _steerCurve;
 
@@ -15,32 +15,53 @@ public class CarInputControl : MonoBehaviour
     private float _handbrakeAxis;
     private void Update()
     {
-        _wheelSpeed = car.WheelSpeed;
+        _wheelSpeed = _car.WheelSpeed;
 
         UpdateAxis();
         UpdateTorqueAndBrake();
         UpdateSteer();
         UpdateAutoBreak();
+
+        //Test
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            _car.UpGear();
+        }
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            _car.DownGear();
+        }
+
     }
 
     private void UpdateSteer()
     {
-        car.steerControl = _steerCurve.Evaluate(car.WheelSpeed / car.MaxSpeed) * _horizontalAxis;
+        _car.steerControl = _steerCurve.Evaluate(_car.WheelSpeed / _car.MaxSpeed) * _horizontalAxis;
     }
 
     private void UpdateTorqueAndBrake()
     {
         if (Mathf.Sign(_verticalAxis) == Mathf.Sign(_wheelSpeed) || Mathf.Abs(_wheelSpeed) < 0.5f)
         {
-            car.throttleControl = Input.GetAxis("Vertical");
-            car.brakeControl = 0;
+            _car.throttleControl = Mathf.Abs(_verticalAxis);
+            _car.brakeControl = 0;
         }
         else
         {
-            car.throttleControl = 0;
-            car.brakeControl = _brakeCurve.Evaluate(_wheelSpeed / car.MaxSpeed);
+            _car.throttleControl = 0;
+            _car.brakeControl = _brakeCurve.Evaluate(_wheelSpeed / _car.MaxSpeed);
         }
 
+        //Gears
+        if (_verticalAxis < 0 && _wheelSpeed > -0.5f && _wheelSpeed <= 0.5f)
+        {
+            _car.ShiftToReverseGear();
+        }
+
+        if (_verticalAxis > 0 && _wheelSpeed > -0.5f && _wheelSpeed < 0.5f)
+        {
+            _car.ShiftToFirstGear();
+        }
     }
 
     private void UpdateAxis()
@@ -54,7 +75,7 @@ public class CarInputControl : MonoBehaviour
     {
         if (Input.GetAxis("Vertical") == 0)
         {
-            car.brakeControl = _brakeCurve.Evaluate(_wheelSpeed / car.MaxSpeed) * _autoBrakeStrength;
+            _car.brakeControl = _brakeCurve.Evaluate(_wheelSpeed / _car.MaxSpeed) * _autoBrakeStrength;
         }
     }
 }
